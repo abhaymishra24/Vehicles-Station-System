35c9960fb7c7884805c41e5bafba74fff38db03b
Abhay Mishra <techabhay06@gmail.com>
2024-09-03 11:57:28 +0530

    Add new section file

---
+++ Speakfeature.py
@@ -0,0 +1,29 @@
+
+# New file for speak feature 
+
+import pyttsx3
+engine = pyttsx3.init()  
+
+ 
+rate = engine.getProperty('rate')   
+print (rate)                         
+engine.setProperty('rate', 200)      
+
+
+ 
+volume = engine.getProperty('volume')   
+print (volume)                          
+engine.setProperty('volume',5.0)    
+
+ 
+voices = engine.getProperty('voices')       
+engine.setProperty('voice', voices[0].id)    
+
+engine.say("Your Payment Successfully")
+engine.say("Your bike is unlock")
+engine.say("Thankyou for your experince, Please visit once again")
+engine.runAndWait()
+engine.stop()
+ 
+engine.save_to_file('Hello World', 'test.mp3')
+engine.runAndWait()
