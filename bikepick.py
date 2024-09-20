d573b0488929195bc9298212d32483456f626268
Abhay Mishra <techabhay06@gmail.com>
2024-09-20 20:25:30 +0530

    Add new code

--- bikepick.py
+++ bikepick.py
@@ -8,20 +8,25 @@ bike_prices = {
     "Road Bike": 700,
     "Hybrid Bike": 600,
     "Electric Bike": 1200,
-    "BMX Bike": 300
+    "BMX Bike": 300,
+    "Sports Bike": 350,
+    "Genral Bike": 200
 }
 
 def display_bike_prices():
     """Display the prices of all bike models."""
+
     print("Bike Prices:")
     for bike, price in bike_prices.items():
-        print(f"{bike}: ${price}")
+
+        print(f"{bike}: Rs:{price}")
 
 def update_bike_price(bike_model, new_price):
     """Update the price of a specific bike model."""
+
     if bike_model in bike_prices:
         bike_prices[bike_model] = new_price
-        print(f"Updated {bike_model} price to ${new_price}.")
+        print(f"Updated {bike_model} price to Rs:{new_price}.")
     else:
         print(f"{bike_model} not found in the bike list.")
 
@@ -30,7 +35,7 @@ def main():
     display_bike_prices()
     
     # Update a bike price as an example
-    update_bike_price("Mountain Bike", 550)
+    update_bike_price("Electric Bike", 650)
     
     # Display the updated bike prices
     display_bike_prices()
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,13 @@
+print("Bike Prices:")
+#     for bike, price in bike_prices.items():
+
+#         print(f"{bike}: Rs:{price}")
+
+# def update_bike_price(bike_model, new_price):
+#     """Update the price of a specific bike model."""
+
+#     if bike_model in bike_prices:
+#         bike_prices[bike_model] = new_price
+#         print(f"Updated {bike_model} price to Rs:{new_price}.")
+#     else:
+#         print(f"{bike_model} not found in the bike list.")
